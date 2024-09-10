d113cc779006b2b76df68b56206c1ff92e134d8b
Md. Abir mahmud <abirmahmud5665@gmail.com>
2024-09-10 08:58:55 +0600

    [done] set up ui component library

---
+++ src/app/(root_layout)/layout.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+const Homepage = ({ children }: { children: ReactNode }) => {
+  return <>{children}</>;
+};
+
+export default Homepage;
---
+++ src/app/(root_layout)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const page = () => {
+  return (
+    <div className="container">
+      <h2>Welcome</h2>
+      <Button variant={"link"}>Click me</Button>
+    </div>
+  );
+};
+
+export default page;
